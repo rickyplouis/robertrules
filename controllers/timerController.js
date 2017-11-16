b330d06877a62ade11127705976043d3d8baeac7
Ricardo <ricardoplouis@gmail.com>
2017-11-16 13:25:58 -0600

    Rewrote tests to work without babel due to nextjs compilation issues

--- controllers/timerController.js
+++ controllers/timerController.js
@@ -1,14 +1,16 @@
-export function displaySeconds(inputSeconds){
-  let seconds = inputSeconds % 60;
-  return seconds < 10 ? '0' + seconds : seconds;
+var timer = {
+  displaySeconds(inputSeconds){
+    let seconds = inputSeconds % 60;
+    return seconds < 10 ? '0' + seconds : seconds;
+  },
+  displayMinutes(seconds){
+    return Math.floor( seconds / 60);
+  },
+  convertTimeToSeconds(timerObject={minutes: 0, seconds:0}){
+    let minutes = Math.abs(parseInt(timerObject.minutes || 0));
+    let seconds = Math.abs(parseInt(timerObject.seconds || 0));
+    return minutes * 60 + seconds;
+  }
 }
 
-export function displayMinutes(seconds){
-  return Math.floor( seconds / 60);
-}
-
-export function convertTimeToSeconds(timerObject={minutes: 0, seconds:0}){
-  let minutes = Math.abs(parseInt(timerObject.minutes || 0));
-  let seconds = Math.abs(parseInt(timerObject.seconds || 0));
-  return minutes * 60 + seconds;
-}
+module.exports = timer;
--- tests/timer.spec.js
+++ tests/timer.spec.js
@@ -1,5 +1,5 @@
-const expect = require('chai').expect;
-import { convertTimeToSeconds } from '../controllers/timerController'
+var expect = require('chai').expect;
+var timer = require('../controllers/timerController');
 
 var mockTimers = [
   {
@@ -24,44 +24,44 @@ var mockTimers = [
   }
 ]
 
-describe('timer.spec.js', () => {
+describe('timer.spec.js', function(){
 
-  describe('convertTimeToSeconds()', () => {
+  describe('convertTimeToSeconds()', function() {
 
-    it('should handle no input timer', () => {
-      expect(convertTimeToSeconds()).to.equal(0)
+    it('should handle no input timer', function() {
+      expect(timer.convertTimeToSeconds()).to.equal(0)
     })
 
-    it('should convert seconds to seconds', () => {
-      expect(convertTimeToSeconds(mockTimers[0])).to.equal(30)
+    it('should convert seconds to seconds', function() {
+      expect(timer.convertTimeToSeconds(mockTimers[0])).to.equal(30)
     })
 
-    it('should convert minutes to seconds', () => {
-      expect(convertTimeToSeconds(mockTimers[1])).to.equal(120)
+    it('should convert minutes to seconds', function() {
+      expect(timer.convertTimeToSeconds(mockTimers[1])).to.equal(120)
     })
 
-    it('should handle high input for minutes and seconds', () => {
-      expect(convertTimeToSeconds(mockTimers[2])).to.equal(3300)
+    it('should handle high input for minutes and seconds', function() {
+      expect(timer.convertTimeToSeconds(mockTimers[2])).to.equal(3300)
     })
 
-    it('should handle string inputs for minutes and seconds', () => {
-      expect(convertTimeToSeconds(mockTimers[3])).to.equal(140)
+    it('should handle string inputs for minutes and seconds', function() {
+      expect(timer.convertTimeToSeconds(mockTimers[3])).to.equal(140)
     })
 
-    it('should handle negative inputs for minutes and seconds', () => {
-      expect(convertTimeToSeconds(mockTimers[4])).to.equal(130)
+    it('should handle negative inputs for minutes and seconds', function() {
+      expect(timer.convertTimeToSeconds(mockTimers[4])).to.equal(130)
     })
 
-    it('should handle missing seconds input', () => {
-      expect(convertTimeToSeconds({minutes:2})).to.equal(120);
+    it('should handle missing seconds input', function() {
+      expect(timer.convertTimeToSeconds({minutes:2})).to.equal(120);
     })
 
-    it('should handle missing minutes input', () => {
-      expect(convertTimeToSeconds({seconds:68})).to.equal(68);
+    it('should handle missing minutes input', function() {
+      expect(timer.convertTimeToSeconds({seconds:68})).to.equal(68);
     })
 
-    it('should handle fractions for minutes and seconds', () => {
-      expect(convertTimeToSeconds({minutes: 2.8, seconds: 60.9})).to.equal(180)
+    it('should handle fractions for minutes and seconds', function() {
+      expect(timer.convertTimeToSeconds({minutes: 2.8, seconds: 60.9})).to.equal(180)
     })
   })
 
